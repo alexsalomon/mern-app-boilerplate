b03d0e98673e1168a40fc04bbaa1f604585b3c79
Alex Salomon <alexsalomon.th@gmail.com>
2018-11-24 12:50:03 +0000

    npm install strv/eslint-config-js and add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    '@strv/javascript/environments/nodejs/v10',
+    '@strv/javascript/environments/nodejs/optional',
+    '@strv/javascript/environments/mocha/recommended',
+    '@strv/javascript/coding-styles/recommended',
+  ],
+
+  overrides: [{
+    files: [
+      'test/**',
+    ],
+
+    rules: {
+      'func-names': 'off',
+    },
+  }],
+}
