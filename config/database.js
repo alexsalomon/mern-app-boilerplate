f1eda3c9dd6d066e26387e507f9c0847c7acaa93
Alex Salomon <alexsalomon.th@gmail.com>
2018-11-30 03:11:09 +0000

    Fix database deprecated warnings

--- config/database.js
+++ config/database.js
@@ -5,7 +5,12 @@ const config = require('./settings')
 
 function init() {
   mongoose.set('debug', config.db.debug)
+
+  // Fix deprecated warnings
+  mongoose.set('useFindAndModify', false)
+  mongoose.set('useCreateIndex', true)
   mongoose.set('useNewUrlParser', true)
+
   mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`)
   mongoose.connection.on('error', err => {
     if (err) {
