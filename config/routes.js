e0fea874995ba63a81f2c149cc0451d359d05c3f
Alex Salomon <alexsalomon.th@gmail.com>
2018-11-30 15:37:52 +0000

    Error handler and 404 routing for unknown resources

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,26 @@
 'use strict'
 
+const HttpStatus = require('http-status')
+const ApiError = require('http-errors')
 const express = require('express')
 
 const router = new express.Router()
 
 router.get('/', (req, res) => {
   res.status(200).send('API works.')
+})
+
+router.all('*', (req, res, next) => {
+  next(new ApiError.NotFound('Resource not found.'))
+})
+
+router.use(function handleErrors(err, req, res, next) {
+  res.status(err.status || HttpStatus.INTERNAL_SERVER_ERROR).json({
+    type: err.type || err.name,
+    message: err.message || HttpStatus['500_NAME'],
+  })
+
+  next(err)
 })
 
 module.exports = router
