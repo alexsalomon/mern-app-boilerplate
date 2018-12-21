f36dd163f27e47579f1916576319efb0d5382d7c
Alex Salomon <alexsalomon.th@gmail.com>
2018-12-21 13:08:57 +0000

    Small tweak on the routesUtil function to allow routes to change the success response status code of an endpoint

--- source/util/routes.util.js
+++ source/util/routes.util.js
@@ -1,20 +1,24 @@
+const HttpStatus = require('http-status')
+
 /**
  * Handles controller execution, removing the need to wrap every controller in a try/catch
  * block, handle errors and send a json response upon successful execution.
  * Additionally, we can send just the necessary parameters to be handled by the controller.
  * @param {Promise} promise The controller handler.
  * @param {function(req=, res=, next=)} params The desired controller parameters.
+ * @param {Promise} successStatus The controller handler.
  * @returns {json} The response to the client
  */
-const controllerHandler = (promise, params) => async (req, res, next) => {
-  const boundParams = params ? params(req, res, next) : []
-  try {
-    const result = await promise(...boundParams)
-    return res.json(result || { message: 'OK' })
-  } catch (err) {
-    return next(err)
+const controllerHandler = (promise, params, successStatus = HttpStatus.OK) =>
+  async (req, res, next) => {
+    const boundParams = params ? params(req, res, next) : []
+    try {
+      const result = await promise(...boundParams)
+      return res.status(successStatus).json(result || { message: 'OK' })
+    } catch (err) {
+      return next(err)
+    }
   }
-}
 
 
 module.exports = { controllerHandler }
