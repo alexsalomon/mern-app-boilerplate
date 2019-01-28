39f1f122d933f5fca3bd339ee659ee9aade7ffda
Alex Salomon <alexsalomon.th@gmail.com>
2019-01-28 18:48:11 +0000

    Update error handler code documentation

--- source/services/errorHandler/errorHandler.services.js
+++ source/services/errorHandler/errorHandler.services.js
@@ -2,12 +2,28 @@ const validator = require('express-validation')
 const logger = require('../logger')
 const errors = require('../../util/errors')
 
+/**
+ * Substitutes express' default error handler middleware.
+ * @param {object} err The error object.
+ * @param {object} req Express' request object.
+ * @param {object} res Express' response object.
+ * @param {function} next Express' next middleware in the execution chain.
+ * @returns {void}
+ */
 function handleError(err, req, res, next) {
   err = convertKnownErrors(err)
   handleOperationalError(err, req, res, next)
   handleProgrammerError(err)
 }
 
+/**
+ * Handles recoverable errors.
+ * @param {object} err The error object.
+ * @param {object} req Express' request object.
+ * @param {object} res Express' response object.
+ * @param {function} next Express' next middleware in the execution chain.
+ * @returns {void}
+ */
 function handleOperationalError(err, req, res, next) {
   if (err.isOperational) {
     logger.warn(err.message, err)
@@ -15,10 +31,18 @@ function handleOperationalError(err, req, res, next) {
   }
 }
 
+/**
+ * Handles unexpected errors.
+ * @param {object} err The error object.
+ * @param {object} req Express' request object.
+ * @param {object} res Express' response object.
+ * @param {function} next Express' next middleware in the execution chain.
+ * @returns {void}
+ */
 function handleProgrammerError(err) {
   if (!err.isOperational) {
     logger.error(err.message, err)
-    // Programmer errors leave the application in an unknown state, let's kill it.
+    // Programmer errors leave the application in an unknown state, so let's kill it.
     process.exit(1) /* eslint-disable-line no-process-exit */
   }
 }
@@ -26,8 +50,8 @@ function handleProgrammerError(err) {
 /**
  * Convert third-party library errors into our own custom errors. This is needed so
  * that we can call custom methods from ExtendableError (such as getFormattedError).
- * @param {Object} err the error being handled.
- * @returns {Object} the converted error.
+ * @param {object} err the error being handled.
+ * @returns {object} the converted error.
  */
 function convertKnownErrors(err) {
   if (err instanceof validator.ValidationError) {
@@ -35,5 +59,6 @@ function convertKnownErrors(err) {
   }
   return err
 }
+
 
 module.exports = { handleError }
