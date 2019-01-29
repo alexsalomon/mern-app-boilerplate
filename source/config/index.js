91e314cbed9a70368f11522133290493363f5098
Alex Salomon <alexsalomon.th@gmail.com>
2019-01-29 14:37:25 +0000

    Change dotenv for dotenv-safe in order to ensure .env.example variables are enforced

--- source/config/index.js
+++ source/config/index.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv-safe')
 const convict = require('convict')
 const configurations = require('./components')
 
